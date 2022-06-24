c363fea2674f985972ad9226c9b0e852ce7ed6cb
David Maas <contact@pathogenstudios.com>
2022-06-24 14:33:30 -0500

    Created demonstration.

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo demonstrates an issue with some Source Link implementations where a `#` symbol in the file path causes issues.
---
+++ SourceLinkHashTagIssue/Program.cs
@@ -0,0 +1,10 @@
+﻿using Biohazrd;
+using Biohazrd.Expressions;
+
+// For `FromInt32`, SourceLink should find https://raw.githubusercontent.com/MochiLibraries/Biohazrd/9e11ce098fa067438d7fbd032fb9a9cebffaab19/Biohazrd/Expressions/IntegerConstant.cs
+// Works as expected in all scenarios I tested, just including it to show that SourceLink works for this library in general.
+IntegerConstant.FromInt32(42);
+
+// For `Create`, SourceLink should find https://raw.githubusercontent.com/MochiLibraries/Biohazrd/main/Biohazrd/%23TypeReferences/TranslatedTypeReference.cs
+// Source Link fails here in some scenarios because the `#` is (presumably) not properly escaped as `%23`
+TranslatedTypeReference.Create(default(DeclarationId));
